2474903dbdde516f970fff9c4b7c1344112bdb59
Kabeer Parihar <80457185+Kabeer75@users.noreply.github.com>
2021-07-06 15:14:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+if (message.content === "/ping") {
+message.channel.send(`⏱| **${client.ws.ping}ms** Latency!`)
+}
+/// A simple command for your bot which can check the Ping Latency of the Bot with the user. Paste this code below as a new command. Prefix is customizable. Simple ping command without command or event handler///
